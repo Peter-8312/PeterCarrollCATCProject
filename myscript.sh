e165d7112883974c374a05af702ff2dc7080cb0c
Peter Carroll <peterccarroll@gmail.com>
2024-04-29 17:05:34 +0000

    Created a basic shell script to execute the starred commands

---
+++ myscript.sh
@@ -0,0 +1,31 @@
+# This script displays various system information
+
+echo "Current Date and Time:"
+date
+
+echo "Hostname:"
+hostname
+
+echo "System Architecture:"
+arch
+
+echo "System Information:"
+uname -a
+
+echo "System Uptime:"
+uptime
+
+echo "Current User:"
+whoami
+
+echo "Logged-in Users:"
+who
+
+echo "Detailed Information about Logged-in Users:"
+w
+
+echo "Real-Time System Resource Usage:"
+top
+
+echo "Command History:"
+history
