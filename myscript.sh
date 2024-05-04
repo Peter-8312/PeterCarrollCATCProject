9453d712a1caf23309e230e77ccf6fc261e31deb
Peter Carroll <peterccarroll@gmail.com>
2024-05-04 16:01:50 +0000

    I realised I had not included a shebang in my code. While it didn't appear to cause issues, I know understand it is proper protocol. So file edited and updated.

--- myscript.sh
+++ myscript.sh
@@ -1,34 +1,60 @@
+#!/bin/bash
 # This script displays various system information
 
 #Redirect output of each command to text file
 exec >> peterCarroll.txt
 
+echo "Shellscript for CATC Project"
+
+echo
+
+
+echo
+
 echo "Current Date and Time:"
 date
+
+echo
 
 echo "Hostname:"
 hostname
 
+echo
+
 echo "System Architecture:"
 arch
+
+echo
 
 echo "System Information:"
 uname -a
 
+echo
+
 echo "System Uptime:"
 uptime
+
+echo
 
 echo "Current User:"
 whoami
 
+echo
+
 echo "Logged-in Users:"
 who
+
+echo
 
 echo "Detailed Information about Logged-in Users:"
 w
 
+echo
+
 echo "Real-Time System Resource Usage:"
 top
+
+echo
 
 echo "Command History:"
 history
